a60c4ba85b9b9321df553e845e1df30ec068249f
Noel De Martin <noeldemartin@gmail.com>
2018-05-05 11:37:17 +0200

    Update publication script

--- app/templates/spa/scripts/publish.sh
+++ app/templates/spa/scripts/publish.sh
@@ -1,19 +1,23 @@
-#!/bin/sh
+#!/usr/bin/env sh
 
-ROOT=$(cd $(dirname "$0") && cd .. && pwd)
-NAME=`basename $ROOT`
-TMP_DIR="/tmp/$NAME"
-
-cd $ROOT
 REPO_URL=`git remote get-url origin`
-npm run prod
-rm -rf $TMP_DIR
-mkdir $TMP_DIR
-cp index.html $TMP_DIR
-cp build $TMP_DIR -r
 
-cd $TMP_DIR
+# abort on errors
+set -e
+
+# build
+npm run prod
+rm dist -rf && mkdir dist
+cp index.html dist
+cp build dist -r
+cd dist
+
+# create repository
 git init
-git add .
+git add -A
 git commit -m "Publish website"
+
+# push updates
 git push $REPO_URL master:gh-pages --force
+
+cd -
